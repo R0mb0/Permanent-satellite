89d12ce7dee06ad766fdbdb9264cb70e75542f23
RomboUrbex <f.rombaldoni@campus.uniurb.it>
2020-10-15 10:15:18 +0200

    classi corrette e preparata la classe nodo

---
+++ SatellitePermanente/SatellitePermanente/LogicAndMath/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SatellitePermanente.LogicAndMath
+{
+    interface Node
+    {
+        public decimal GetDistance();
+
+        public DateTime GetTimeDiffrence();
+
+        public decimal GetSpeed();
+
+        public int? GetAltitudeDifference();
+    }
+}
---
+++ SatellitePermanente/SatellitePermanente/LogicAndMath/NodeImpl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SatellitePermanente.LogicAndMath
+{
+    class NodeImpl: Node
+    {
+        private readonly Point pointA;
+        private readonly Point pointB;
+
+        public NodeImpl(Point pointA, Point pointB)
+        {
+            this.pointA = pointA;
+            this.pointB = pointB;
+        }
+
+
+    }
+}
